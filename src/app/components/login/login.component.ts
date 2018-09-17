d9b6ae1d4b9e8d74620f0506f3bb3c3f1fd68c60
CarlosCabreraGentile <carloscgentile@gmail.com>
2018-09-17 12:44:46 -0300

    login and logout with jwt finished

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { FlashMessagesService } from 'angular2-flash-messages';
+import { AuthService } from '../../services/auth.service';
 
 @Component({
   selector: 'app-login',
@@ -6,10 +9,32 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
+  username: String;
+  password: String;
 
-  constructor() { }
+  constructor(private AuthService: AuthService,
+    private FlashMessagesService: FlashMessagesService,
+    private Router: Router) { }
 
   ngOnInit() {
   }
 
+  onLoginSubmit() {
+    const user = {
+      username: this.username,
+      password: this.password
+    }
+
+    this.AuthService.authenticateUser(user)
+      .subscribe(data => {
+        if (data.success) {
+          this.AuthService.storeUserData(data.token, data.user);
+          this.FlashMessagesService.show('Logged In', { cssClass: 'alert-success', timeout: 4000 });
+          this.Router.navigate(['/dashboard']);
+        } else {
+          this.FlashMessagesService.show(data.msg, { cssClass: 'alert-danger', timeout: 4000 });
+          this.Router.navigate(['/login']);
+        }
+      });
+  }
 }
--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
-import { Http, HttpModule, Headers,  } from '@angular/http';
-import { HttpClientModule } from '@angular/common/http'; 
+import { Http, HttpModule, Headers, } from '@angular/http';
+import { HttpClientModule } from '@angular/common/http';
 import { map } from 'rxjs/operators';
 // import  'rxjs/add/operator/map';
 import { catchError } from 'rxjs/operators'
@@ -12,17 +12,41 @@ import { Observable } from 'rxjs';
 
 export class AuthService {
   authToken: any;
-  user: any;   
+  user: any;
 
   constructor(private http: Http) { }
 
-  registerUser(user){
+  registerUser(user) {
     let headers = new Headers();
     headers.append('Content-Type', 'application/json');
-    return this.http.post('http://localhost:3000/users/register', user, {headers:headers})
-    // .map(res => res.json());
-    .pipe(map((response: any) => response.json()))
-		.pipe(catchError(this.handleError));
+    return this.http.post('http://localhost:3000/users/register', user, { headers: headers })
+      // .map(res => res.json());
+      .pipe(map((response: any) => response.json()))
+      .pipe(catchError(this.handleError));
+  }
+
+  authenticateUser(user) {
+    let headers = new Headers();
+    headers.append('Content-Type', 'application/json');
+    return this.http.post('http://localhost:3000/users/authenticate', user, { headers: headers })
+      // .map(res => res.json());
+      .pipe(map((response: any) => response.json()))
+      .pipe(catchError(this.handleError));
+  }
+
+  storeUserData(token, user){
+    //jwt look for this path (id_token) automatically 
+    localStorage.setItem('id_token', token);
+    //local storage only storage a string
+    localStorage.setItem('user', JSON.stringify(user));
+    this.authToken = token;
+    this.user = user;
+  }
+
+  logout(){
+    this.authToken = null;
+    this.user = null;
+    localStorage.clear();
   }
 
   /**
@@ -30,9 +54,9 @@ export class AuthService {
 	 * @param error
 	 * @returns {any}
 	 */
-	private handleError(error: any) {
-		console.error(error);
-		return Observable.throw(error.json() || 'Server error');
-	}
+  private handleError(error: any) {
+    console.error(error);
+    return Observable.throw(error.json() || 'Server error');
+  }
 
 }
